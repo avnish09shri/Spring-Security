1ebdfb9666ba19c4337e535542593c228740bcfd
AVNISH\avnis <avnish.shrivastava@tatvasoft.com>
2023-08-21 18:10:01 +0530

    initial commit

---
+++ SpringSecurityBasic-01/src/main/java/com/springsecurity/controller/SpringSecurityBasic01Application.java
@@ -0,0 +1,12 @@
+package com.springsecurity.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBasic01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityBasic01Application.class, args);
+    }
+}
