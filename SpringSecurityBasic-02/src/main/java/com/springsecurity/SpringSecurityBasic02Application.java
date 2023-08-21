9c6dd3e479b0dc1abf7516f68b83ef802192d6cd
AVNISH\avnis <avnishshrivastava280@outlook.com>
2023-08-21 18:22:13 +0530

    section-02 commit

---
+++ SpringSecurityBasic-02/src/main/java/com/springsecurity/SpringSecurityBasic02Application.java
@@ -0,0 +1,11 @@
+package com.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBasic02Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityBasic02Application.class, args);
+    }
+}
