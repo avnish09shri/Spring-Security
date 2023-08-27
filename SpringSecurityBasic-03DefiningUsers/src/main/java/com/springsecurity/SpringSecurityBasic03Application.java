3858ca7a20f9ff5464611a96ba90d0495aca9c58
AVNISH\avnis <avnishshrivastava280@outlook.com>
2023-08-27 17:38:57 +0530

    section-03 initial commit

---
+++ SpringSecurityBasic-03DefiningUsers/src/main/java/com/springsecurity/SpringSecurityBasic03Application.java
@@ -0,0 +1,13 @@
+package com.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBasic03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityBasic03Application.class);
+    }
+
+}
