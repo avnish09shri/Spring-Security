8bcbf59c8017467aedcf6cbbf737764593299d82
AVNISH\avnis <avnishshrivastava280@outlook.com>
2023-09-19 16:28:48 +0530

    section-04 end commit

--- SpringSecurityBasic-04PasswordEncoder/src/main/java/com/springsecurity/config/SecurityConfig.java
+++ SpringSecurityBasic-04PasswordEncoder/src/main/java/com/springsecurity/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -53,6 +54,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder getPasswordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
--- SpringSecurityBasic-04PasswordEncoder/src/main/java/com/springsecurity/service/CustomerServiceImpl.java
+++ SpringSecurityBasic-04PasswordEncoder/src/main/java/com/springsecurity/service/CustomerServiceImpl.java
@@ -5,16 +5,24 @@ import com.springsecurity.model.response.CustomerResponse;
 import com.springsecurity.repository.CustomerRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerServiceImpl implements CustomerService{
 
-    @Autowired
-    private CustomerRepository customerRepository;
+
+    private final CustomerRepository customerRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
+        this.customerRepository = customerRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     public CustomerResponse saveCustomer(Customer customer) {
+        customer.setPassword(passwordEncoder.encode(customer.getPassword()));
         Customer newCustomer = customerRepository.save(customer);
         return mapToResponse(newCustomer);
     }
